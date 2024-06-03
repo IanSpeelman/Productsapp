530336c84beaa0b92c26c6e8b9ad021ac5504b2c
Ian Speelman <ianspeelman@gmail.com>
2024-06-03 13:28:32 +0200

    added the edit route for products

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const port = 3001
+const methodOverride = require("method-override")
 
 const Product = require("./models/product")
 
 app.set("views", `${__dirname}/views`)
 app.set("view engine", "ejs")
+
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 mongoose.connect("mongodb://localhost:27017/productstore")
       .then(() => console.log("database connection success"))
       .catch(err => console.log("database connection error", err))
@@ -20,6 +23,12 @@ app.get("/products", async (req,res) => {
 app.get("/products/new", (req,res) => {
       res.render("./products/new",{})
 })
+app.get("/products/:id/edit", async(req,res) => {
+      const {id} = req.params;
+      const product = await Product.findById(id)
+      console.log(product)
+      res.render("./products/edit", {product})
+})
 app.get("/products/:id", async (req,res) => {
       const {id} = req.params;
       const product = await Product.findById(id)
@@ -29,6 +38,11 @@ app.post("/products", async (req,res) => {
       const newProduct = new Product(req.body)
       await newProduct.save()
       res.redirect(302, `/products/${newProduct.id}`)
+})
+app.put("/products/:id", async (req, res) => {
+      const {id} = req.params
+      await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+res.redirect(`/products/${id}`)
 })
 
 app.listen(port, () => console.log(`app listening on: http://localhost:${port}`))
