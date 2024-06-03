45f7b233963386aa5511a7a60cd415ae4110f434
Ian Speelman <ianspeelman@gmail.com>
2024-06-03 14:26:41 +0200

    add delete route, generate options for categories in create and edit, add sort by categorie functionality

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const methodOverride = require("method-override")
 
 const Product = require("./models/product")
 
+const categories = ["fruit" ,"vegetable" , "dairy"]
+
 app.set("views", `${__dirname}/views`)
 app.set("view engine", "ejs")
 
@@ -21,13 +23,19 @@ app.get("/products", async (req,res) => {
       res.render("./products/index", {products})
 })
 app.get("/products/new", (req,res) => {
-      res.render("./products/new",{})
+      res.render("./products/new",{categories})
+})
+app.get("/products/categories/:cat", async (req,res) => {
+      const {cat} =req.params
+      const products = await Product.find({category: cat})
+      console.log(products)
+      res.render("./products/index", {products})
 })
 app.get("/products/:id/edit", async(req,res) => {
       const {id} = req.params;
       const product = await Product.findById(id)
       console.log(product)
-      res.render("./products/edit", {product})
+      res.render("./products/edit", {product, categories})
 })
 app.get("/products/:id", async (req,res) => {
       const {id} = req.params;
@@ -43,6 +51,11 @@ app.put("/products/:id", async (req, res) => {
       const {id} = req.params
       await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
 res.redirect(`/products/${id}`)
+})
+app.delete("/products/:id", async (req,res) => {
+      const { id } = req.params
+      const deleted = await Product.findByIdAndDelete(id)
+      res.redirect("/products")
 })
 
 app.listen(port, () => console.log(`app listening on: http://localhost:${port}`))
