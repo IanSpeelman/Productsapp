af4661d6e1923e0028028063795fe1a2cd9cc7b3
Ian Speelman <ianspeelman@gmail.com>
2024-06-03 09:12:08 +0200

    add the product route and the show product route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const port = 3001
+
+const Product = require("./models/product")
 app.set("views", `${__dirname}/views`)
 app.set("view engine", "ejs")
 mongoose.connect("mongodb://localhost:27017/productstore")
@@ -9,5 +11,13 @@ mongoose.connect("mongodb://localhost:27017/productstore")
       .catch(err => console.log("database connection error", err))
 
 
-
+app.get("/products", async (req,res) => {
+      const products = await Product.find()
+      res.render("./products/index", {products})
+})
+app.get("/products/:id", async (req,res) => {
+      const {id} = req.params;
+      const product = await Product.findById(id)
+      res.render("./products/show", {product})
+})
 app.listen(port, () => console.log(`app listening on: http://localhost:${port}`))
